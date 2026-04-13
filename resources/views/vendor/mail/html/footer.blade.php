9bbc056fba932e251ea051ddcb4198aed8c40038
Dansoy160309 <dansoy@example.com>
2026-04-14 00:15:29 +0800

    feat(email): add header and footer templates for email notifications

---
+++ resources/views/vendor/mail/html/footer.blade.php
@@ -0,0 +1,11 @@
+<tr>
+<td>
+<table class="footer" align="center" width="570" cellpadding="0" cellspacing="0" role="presentation">
+<tr>
+<td class="content-cell" align="center">
+{{ Illuminate\Support\Str::of('© '.date('Y').' E-Barangay Health. '.__('All rights reserved.'))->inlineMarkdown()->toHtmlString() }}
+</td>
+</tr>
+</table>
+</td>
+</tr>
---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,7 @@
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block; text-decoration: none;">
+<img src="{{ url('/assets/images/LOGO%20(2).png') }}" class="logo" alt="E-Barangay Health Logo" style="max-height: 64px; width: auto;">
+</a>
+</td>
+</tr>
